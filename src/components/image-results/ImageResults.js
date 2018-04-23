234c1ee577301c2fb9615e682daa0973a7127f9b
Jarkko <jarkkohei@luukku.com>
2018-04-23 18:18:48 +0300

    Added the opening and closing the zooming dialog

--- src/components/image-results/ImageResults.js
+++ src/components/image-results/ImageResults.js
@@ -8,40 +8,70 @@ import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 
 class ImageResults extends Component {
-  render() {
 
-    let imageListContent;
-    const { images } = this.props;
-
-    if(images) {
-        imageListContent = (
-            <GridList cols={ 3 }>
-                { images.map(img => (
-                    <GridTile
-                        title={ img.tags }
-                        key={ img.id }
-                        subtitle={ <span>by <strong>{ img.user }</strong></span> }
-                        actionIcon={ <IconButton><ZoomIn color="white" /></IconButton> }
-                    >
-                        <img src={ img.largeImageURL } alt=""/>
-                    </GridTile>
-                )) }
-            </GridList>
-        )
-    } else {
-        imageListContent = null
+    state = {
+        open: false,
+        currentImage: ''
     }
 
-    return (
-        <div>
-            { imageListContent }
-        </div>
-    )
-  }
+    handleOpen = img => {
+        this.setState({ open: true , currentImage: img });
+    }
+
+    handleClose = () => {
+        this.setState({ open: false });
+    }
+
+    render() {
+
+        let imageListContent;
+        const { images } = this.props;
+
+        if(images) {
+            imageListContent = (
+                <GridList cols={ 3 }>
+                    { images.map(img => (
+                        <GridTile
+                            title={ img.tags }
+                            key={ img.id }
+                            subtitle={ <span>by <strong>{ img.user }</strong></span> }
+                            actionIcon={ 
+                                <IconButton onClick={ () => this.handleOpen(img.largeImageURL) }>
+                                    <ZoomIn color="white" />
+                                </IconButton> 
+                            }
+                        >
+                            <img src={ img.largeImageURL } alt=""/>
+                        </GridTile>
+                    )) }
+                </GridList>
+            )
+        } else {
+            imageListContent = null
+        }
+
+        const actions = [
+            <FlatButton label="Close" primary={ true } onClick={ this.handleClose } />
+        ];
+
+        return (
+            <div>
+                { imageListContent }
+                <Dialog 
+                    actions={ actions } 
+                    modal={ false }
+                    open={ this.state.open }
+                    onRequestClose={ this.handleClose }
+                >
+                    <img src={ this.state.currentImage } alt="" style={{ width: '100%' }} />
+                </Dialog>
+            </div>
+        )
+    }
 }
 
-ImageResults.propTypes = {
-    images: PropTypes.array.isRequired
-}
+    ImageResults.propTypes = {
+        images: PropTypes.array.isRequired
+    }
 
 export default ImageResults;
